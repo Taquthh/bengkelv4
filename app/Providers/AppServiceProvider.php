35ab712f9bac61cc319b321fafc0f0054fcf7f45
Taquthh <quthbiofficial@gmail.com>
2026-05-11 07:01:52 +0800

    Set Up Deployment Vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,15 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Jika dijalankan di lokal, gunakan http agar tidak error SSL
-        if (config('app.env') === 'local') {
-            URL::forceScheme('http');
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
         }
-
-        // 🔹 Set bahasa Carbon ke Indonesia secara global
-        Carbon::setLocale(config('app.locale', 'id'));
-
-        // 🔹 Set locale sistem untuk tanggal
-        setlocale(LC_TIME, 'id_ID.UTF-8');
     }
 }
